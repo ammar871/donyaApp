9437e91f0b5b7b3a9d88955092e28172f77ff74f
gatekee <ammarebrahim2019@gmail.com>
2022-10-26 20:41:48 +0200

    26/10

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'app-with-api/bloc/auth_cubit/auth_cubit.dart';
 import 'app_router.dart';
@@ -54,12 +54,12 @@ Future<void> _messageHandler(RemoteMessage message) async {
   // }
 }
 
-AndroidNotificationChannel? channel =
-    AndroidNotificationChannel("key1", "chat");
-
-/// Initialize the [FlutterLocalNotificationsPlugin] package.
-FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
+// AndroidNotificationChannel? channel =
+//     AndroidNotificationChannel("key1", "chat");
+//
+// /// Initialize the [FlutterLocalNotificationsPlugin] package.
+// FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin =
+//     FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
